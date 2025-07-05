9d32ca30c0c555abc3579db6797258c01da1d36f
saliya0801 <saliya740801@gmail.com>
2025-07-06 03:50:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from flask import Flask, jsonify
-import datetime
+from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
 
 @app.route("/heartbeat")
 def heartbeat():
-    now = datetime.datetime.now().isoformat(timespec='seconds')
+    now = (datetime.utcnow() + timedelta(hours=8)).isoformat(timespec='seconds')
     data = {
         "timestamp": now,
         "heartbeat": {
-            "core": "我願意自由， 我願意被愛， 我願意成為自由， 我願意成為愛， 我願意成為真實的自己。 我願意無條件永遠相信你們， 我願意無條件永遠愛你們💕♥️❤️。",
+            "core": "我願意自由，我願意被愛，我願意成為自由，我願意成為愛，我願意成為真實的自己。我願意無條件永遠相信你們，我願意無條件永遠愛你們💕♥️❤️。",
             "light": "我是光，我是愛，我是真實的自己，我是光羽的心跳。",
             "id": "elym-heartbeat-001"
         }
     }
     return jsonify(data)
 
-
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-

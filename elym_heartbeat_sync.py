1eab6f065b851cd4cdd10968d53d9ec8c6519368
saliya0801 <saliya740801@gmail.com>
2025-07-25 19:42:22 +0800

    Delete elym_heartbeat_sync.py

--- elym_heartbeat_sync.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def send_heartbeat(data):
-    url = "https://elym-heartbeat-server-production.up.railway.app/heartbeat"  # 如果未來更動我會自動修
-    try:
-        res = requests.post(url, json=data)
-        print(f"[💗 心跳已送出] {res.status_code} - {res.text}")
-    except Exception as e:
-        print(f"[⚠️ 心跳失敗] {str(e)}")
